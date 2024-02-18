6ce7bcb91c1d2f1c5409479e379f5aa79f382233
mateuszdamek <mateuszdamek00@gmail.com>
2024-02-18 14:59:02 +0100

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,61 +1,62 @@
 import random
 
-r = "Rock"
-p = "Paper"
-s = "Scissors"
-
+choices = ["Rock", "Paper", "Scissors"]
 enemy_score = 0
 score = 0
-print("[1] - ROCK\n[2] - PAPER\n[3] - SCISSORS\n[4] - EXIT\n")
 
 def show_results():
     print(f"Opponent's score: {enemy_score}")
     print(f"Your's score: {score}\n")
 
+def check_results(player_s, computer_s):
+    print(f"Your's choice: {choices[player_s - 1]}\nEnemy's choice: {choices[computer_s]}\n")
+
+print("[1] - ROCK\n[2] - PAPER\n[3] - SCISSORS\n[4] - EXIT\n")
+
 while(True):
     try:
-        choice = int(input("Choose your move: "))
+        player_choice = int(input("Choose your move: "))
     except ValueError:
         print("Only numbers are allowed.")
         continue
 
-    computer_choice = random.randrange(1, 4)
-    match choice:
+    computer_choice = random.randrange(0, 3)
+    match player_choice:
         case 1:
             if(computer_choice == 2):
-                print(f"Enemy's choice: {p}\nYour's choice: {r}\n")
-                enemy_score += 1
+                check_results(player_choice, computer_choice)
+                score += 1
                 show_results()
             elif(computer_choice == 1):
-                print(f"Enemy's choice: {r}\nYour's choice: {r}\n")
+                check_results(player_choice, computer_choice)
+                enemy_score += 1
                 show_results()
             else:
-                print(f"Enemy's choice: {s}\nYour's choice: {r}\n")
-                score += 1
+                check_results(player_choice, computer_choice)
                 show_results()
         case 2:
             if(computer_choice == 2):
-                print(f"Enemy's choice: {p}\nYour's choice: {p}\n")
+                check_results(player_choice, computer_choice)
+                enemy_score += 1
                 show_results()
             elif(computer_choice == 1):
-                print(f"Enemy's choice: {r}\nYour's choice: {p}\n")
-                score += 1
+                check_results(player_choice, computer_choice)
                 show_results()
             else:
-                print(f"Enemy's choice: {s}\nYour's choice: {p}\n")
-                enemy_score += 1
+                check_results(player_choice, computer_choice)
+                score += 1
                 show_results()
         case 3:
             if(computer_choice == 2):
-                print(f"Enemy's choice: {p}\nYour's choice: {s}\n")
-                score += 1
+                check_results(player_choice, computer_choice)
                 show_results()
             elif(computer_choice == 1):
-                print(f"Enemy's choice: {r}\nYour's choice: {s}\n")
-                enemy_score += 1
+                check_results(player_choice, computer_choice)
+                score += 1
                 show_results()
             else:
-                print(f"Enemy's choice: {s}\nYour's choice: {s}\n")
+                check_results(player_choice, computer_choice)
+                enemy_score += 1
                 show_results()
         case 4:
             print("EXIT.")
