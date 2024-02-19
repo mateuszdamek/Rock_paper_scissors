662e5ac975207af1404fd2e39a4a9cca5aa29703
mateuszdamek <mateuszdamek00@gmail.com>
2024-02-19 17:37:17 +0100

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,56 +8,50 @@ def show_results():
     print(f"Opponent's score: {enemy_score}")
     print(f"Your's score: {score}\n")
 
-def check_results(player_s, computer_s):
+def check_results(player_s, computer_s, sc, esc):
     print(f"Your's choice: {choices[player_s - 1]}\nEnemy's choice: {choices[computer_s]}\n")
+    global enemy_score 
+    enemy_score += esc
+    global score 
+    score += sc
+    show_results()
 
 print("[1] - ROCK\n[2] - PAPER\n[3] - SCISSORS\n[4] - EXIT\n")
 
 while(True):
     try:
-        player_choice = int(input("Choose your move: "))
+        p_choice = int(input("Choose your move: "))
     except ValueError:
         print("Only numbers are allowed.")
         continue
 
-    computer_choice = random.randrange(0, 3)
-    match player_choice:
+    c_choice = random.randrange(0, 3)
+    match p_choice:
         case 1:
-            if(computer_choice == 2):
-                check_results(player_choice, computer_choice)
-                score += 1
-                show_results()
-            elif(computer_choice == 1):
-                check_results(player_choice, computer_choice)
-                enemy_score += 1
-                show_results()
+            if(c_choice == 2):
+                check_results(p_choice, c_choice, 1, 0)
+            elif(c_choice == 1):
+                check_results(p_choice, c_choice, 0, 1)
             else:
-                check_results(player_choice, computer_choice)
-                show_results()
+                check_results(p_choice, c_choice, 0, 0)
+
         case 2:
-            if(computer_choice == 2):
-                check_results(player_choice, computer_choice)
-                enemy_score += 1
-                show_results()
-            elif(computer_choice == 1):
-                check_results(player_choice, computer_choice)
-                show_results()
+            if(c_choice == 2):
+                check_results(p_choice, c_choice, 0, 1)
+
+            elif(c_choice == 1):
+                check_results(p_choice, c_choice, 0, 0)
             else:
-                check_results(player_choice, computer_choice)
-                score += 1
-                show_results()
+                check_results(p_choice, c_choice, 1, 0)
+
         case 3:
-            if(computer_choice == 2):
-                check_results(player_choice, computer_choice)
-                show_results()
-            elif(computer_choice == 1):
-                check_results(player_choice, computer_choice)
-                score += 1
-                show_results()
+            if(c_choice == 2):
+                check_results(p_choice, c_choice, 0, 0)
+            elif(c_choice == 1):
+                check_results(p_choice, c_choice, 1, 0)
             else:
-                check_results(player_choice, computer_choice)
-                enemy_score += 1
-                show_results()
+                check_results(p_choice, c_choice, 0, 1)
+
         case 4:
             print("EXIT.")
             break
